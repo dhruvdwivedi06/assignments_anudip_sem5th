f130828773f7d80306b7a79dd5ad33a14af2fb39
hello world <dhruvdwivedi0603@gmail.com>
2026-06-06 09:38:13 +0530

    init commit

---
+++ src/LAB_1.java
@@ -0,0 +1,24 @@
+//class Student {
+//    private String studentName;
+//    private String collegeName;
+//    private int studentID;
+//    public static void main(String[] args) {
+//        System.out.println("Successful");
+//    }
+//}
+
+//public class Employee {
+//    private int id;
+//    private int age;
+//    private String name;
+//    private boolean isPermanent;
+//    public static void main(String[] args) {
+//        Employee emp = new Employee();
+//        emp.age = (int)35.5;
+//        emp.id = 101;
+//        emp.name = "dhruv";
+//        emp.isPermanent = true;
+//        System.out.println("success started");
+//    }
+//
+//}
--- src/Student.java
+++
@@ -1,8 +0,0 @@
-class Student {
-    private String studentName;
-    private String collegeName;
-    private int studentID;
-    public static void main(String[] args) {
-        System.out.println("Successful");
-    }
-}
