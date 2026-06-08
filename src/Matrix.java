6f6d67c3e5b63c6ca99a2cf8da664e38dc9d808e
hello world <dhruvdwivedi0603@gmail.com>
2026-06-08 10:58:04 +0530

    init commit

---
+++ src/Matrix.java
@@ -0,0 +1,18 @@
+//Wap to print sum of all 3* 3 matrix elements.
+//
+//import java.util.*;
+//public class Matrix {
+//    public static  void  main(String[] args){
+//        Scanner sc = new Scanner(System.in);
+//        int [][] matrix = new int  [3][3];
+//        int sum = 0;
+//        System.out.println("Enter the number of rows and columns");
+//        for(int i=0;i<matrix.length;i++){
+//            for(int j=0;j<matrix[i].length;j++){
+//                matrix[i][j]=sc.nextInt();
+//                sum+=matrix[i][j];
+//            }
+//        }
+//        System.out.println("The sum is: "+sum);
+//    }
+//}
